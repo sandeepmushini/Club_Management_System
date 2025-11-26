3facc0d2383f3a25a9967230c6ea51d2ed80e2b2
rama subham <ramasubham136@gmail.com>
2025-11-26 23:31:49 +0530

    Remaove dns

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,3 @@
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']); // Force Google DNS
-
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
