541e0fc29b5064cf830a2e8113649d0bbf605272
rama subham <ramasubham136@gmail.com>
2025-11-26 23:11:27 +0530

    Add dns Fix

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Force Google DNS
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
